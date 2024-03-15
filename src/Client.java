7097d099fbda71f905f671143da0cad78d538faf
consultadd-pratham <pratham.consultadd@gmail.com>
2024-03-15 08:04:03 -0400

    Phase 2 updated

--- src/Client.java
+++ src/Client.java
@@ -6,35 +6,43 @@ public class Client {
         User user = new User();
 
         Scanner scn = new Scanner(System.in);
-        System.out.print("Enter username: ");
-        String username = scn.nextLine();
-        System.out.print("Enter password: ");
-        String password = scn.nextLine();
 
-        user.setUsername(username);
-        user.setPassword(password);
+        boolean exit = false;
+        while (!exit) {
+            System.out.print("Enter username: ");
+            String username = scn.nextLine();
+            user.setUsername(username);
 
-        int count = 1;
-        while (true) {
-            try {
-                user.validatePassword();
-                System.out.println("Success");
-                break;
-            } catch (Exception error) {
+            int count = 1;
+            while (true) {
+                System.out.print("Enter password: ");
+                String password = scn.nextLine();
+                user.setPassword(password);
+                try {
+                    user.validatePassword();
+                    System.out.println("Success");
+                    exit = true;
+                    break;
+                } catch (Exception error) {
+                    System.out.println(error.getMessage());
+                    if (count == 3) {
+                        System.out.print("Do you want to continue Yes/No: ");
+                        String yesOrNo = scn.nextLine().toLowerCase();
+                        if (yesOrNo.equals("n") || yesOrNo.equals("no")) {
+                            exit = true;
+                            break;
+                        }
+                    }
+                }
 
-                System.out.println(error.getMessage());
-
-                if (count <= 3) {
-                    System.out.print("Renter password: ");
-                    password = scn.nextLine();
-                    user.setPassword(password);
-                } else {
+                if (count == 6) {
                     break;
                 }
                 count++;
 
             }
         }
+
 
     }
 }
